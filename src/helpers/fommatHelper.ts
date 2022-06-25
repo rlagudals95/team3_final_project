f29db2bb48a13d0b3c9236a8e8484666b26cf6f3
gudals95 <102935090+gudals95@users.noreply.github.com>
2022-06-26 00:11:20 +0900

    feat: 안쓰는 모듈 제거

--- src/helpers/fommatHelper.ts
+++ src/helpers/fommatHelper.ts
@@ -1,4 +1,4 @@
-export const phoneNumberFormatter = (nationalCode, phoneNumber) => {
+export const phoneNumberFormatter = (nationalCode: string, phoneNumber: string) => {
 
   if (phoneNumber.toString().substr(0, 1) === "0") {
     phoneNumber =  phoneNumber.substr(1);
@@ -8,7 +8,7 @@ export const phoneNumberFormatter = (nationalCode, phoneNumber) => {
 };
 
 
-export const emailFormatter = (email, emailDomain) => {
+export const emailFormatter = (email:string, emailDomain:string) => {
 
   if (emailDomain.substr(0, 1) !== "@") {
     emailDomain = "@" + emailDomain;
