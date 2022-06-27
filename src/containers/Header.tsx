7a394626fc7c69c3ddebc83fd0500458c7beb37a
gudals95 <102935090+gudals95@users.noreply.github.com>
2022-06-27 23:17:44 +0900

    feat: useNavigate 추가

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,19 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const goHome = () => {
+    navigate("/");
+  };
+
   return (
     <header className="p-4">
       <div className="flex items-center justify-between bg-opacity-50">
         <div className="mx-2 text-center">
-          <div className="font-extrabold xs:text-3xl md:text-3xl">
-            <a
-              target="_blank"
-              className="hover:text-blue-400"
-              href="https://recoiljs.org/docs/introduction/getting-started"
-            >
-              레시피 프로젝트
-            </a>
-          </div>
+          <div className="font-extrabold xs:text-3xl md:text-3xl"
+            onPointerUp={goHome}
+          >LOGO</div>
         </div>
         <div>메뉴</div>
       </div>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,15 +1,18 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect } from "react";
 import { useRecoilValue } from "recoil";
 
 import Card from "../components/Card";
+import InputV2 from "../components/Input";
 import { recoilStarCountState } from "../recoil/testAtom";
 
-const MainPage = () => {
+const MainPage = ({}) => {
   const recoilStarCount = useRecoilValue(recoilStarCountState);
+
+  useEffect(() => {}, []);
 
   return (
     <>
-      <Card
+      {/* <Card
         cardTitle="카드 타이틀"
         cardDesc="카드 내용"
         styleCustom={{
@@ -19,7 +22,7 @@ const MainPage = () => {
           border: "1px solid grey",
           padding: "8px",
         }}
-      />
+      /> */}
       {/* Suspense를 사용하면 컴포넌트가 렌더링되기 전까지 기다릴 수 있습니다 */}
       <Suspense fallback={<div>로딩중입니다.</div>}>{recoilStarCount}</Suspense>
     </>
